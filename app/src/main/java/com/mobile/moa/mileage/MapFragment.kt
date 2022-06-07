f7ea7f4b5d7c5674fe51bbd229af02578f7c5175
kimehhk <kimehhk@gmail.com>
2022-06-08 01:35:56 +0900

    [ADD] google map fragment

--- app/src/main/java/com/mobile/moa/mileage/MapFragment.kt
+++ app/src/main/java/com/mobile/moa/mileage/MapFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mobile.moa.databinding.FragmentAssetBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.mobile.moa.R
 import com.mobile.moa.databinding.FragmentMapBinding
 
 /* written by keh
-date: 22.05.30 */
+date: 22.06.08 */
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
+    private lateinit var mapView: SupportMapFragment
     lateinit var binding: FragmentMapBinding
 
     override fun onCreateView(
@@ -21,7 +27,19 @@ class MapFragment : Fragment() {
     ): View? {
         binding = FragmentMapBinding.inflate(inflater, container, false)
 
+        mapView = childFragmentManager.findFragmentById(R.id.shop_map) as SupportMapFragment;
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this);
+
+
         return binding.root
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+    }
 }
