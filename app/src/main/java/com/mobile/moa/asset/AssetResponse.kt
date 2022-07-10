33be0d3f8d73ba67e728bdecec6fcd650e892a9a
kimehhk <kimehhk@gmail.com>
2022-07-10 17:53:26 +0900

    [ADD] asset retrofit

---
+++ app/src/main/java/com/mobile/moa/asset/AssetResponse.kt
@@ -0,0 +1,5 @@
+package com.mobile.moa.asset
+
+data class AssetResponse (
+    val total: String
+    )
--- app/src/main/java/com/mobile/moa/mileage/ShopRetrofitInterface.kt
+++ app/src/main/java/com/mobile/moa/mileage/ShopRetrofitInterface.kt
@@ -1,6 +1,5 @@
 package com.mobile.moa.mileage
 
-import com.mobile.moa.model.Shop
 import retrofit2.Call
 import retrofit2.http.GET
 
@@ -10,6 +9,6 @@ date: 22.06.24*/
 //서버 연결 후 사용
 
 interface ShopRetrofitInterface {
-    @GET("shops")
+    @GET("/shops")
     fun getShops(): Call<ShopResponse>
 }
