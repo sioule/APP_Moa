23529cb56e8e4152249d5c4fb5caac8faa647279
kimehhk <kimehhk@gmail.com>
2022-08-11 01:10:38 +0900

    webview & http load

--- app/src/main/java/com/mobile/moa/asset/MyWebViewClient.kt
+++ app/src/main/java/com/mobile/moa/asset/MyWebViewClient.kt
@@ -1,6 +1,18 @@
 package com.mobile.moa.asset
 
+import android.util.Log
+import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MyWebViewClient : WebViewClient() {
+
+//    "code": "2ffd133a-d17a-431d-a6a5",
+//  "scope": "login inquiry transfer",
+//  "client_info": "test",
+//  "state": "b80BLsfigm9OokPTjy03elbJqRHOfGSY"
+
+    override fun onLoadResource(view: WebView?, url: String?) {
+        super.onLoadResource(view, url)
+        Log.d("onLoadResource", url.toString())
+    }
 }
